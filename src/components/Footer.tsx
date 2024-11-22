0cbf9b9c516fd53acf9ca404525802bdedcb3aaa
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2024-11-22 11:14:46 +0100

    Creates a basic placeholder footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,1 +1,22 @@
+import React from "react";
+
 //footer
+
+const Footer = (props: { basic?: boolean }) => {
+  return (
+    <header>
+      <div>
+        <h1>COM STORE</h1>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Contact us</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Footer;
