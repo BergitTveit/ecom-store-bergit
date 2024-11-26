f85698760db87342831b4f249cee4676ccd25dbd
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2024-11-26 16:42:47 +0100

    add png logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
@@ -6,39 +5,37 @@ import {
   faBars,
   faSearch,
 } from "@fortawesome/free-solid-svg-icons";
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-// import styles from './Header.module.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.png";
 
-// check if it will become reusable type, then move
+// import styles from './Header.module.css';
+// check if it will become reusable type in arrow function, then move
 // The header should contain a nav bar as well as a Cart icon component that acts as a button as well as displays the current number of items in the cart.
+
 const Header = (props: { basic?: boolean }) => {
   return (
     <header className="styles.header">
-      <div>
-        <h1>COM {/*LOGO  */}STORE</h1>
+      <div className="">
+        <Link to="/">
+          <img src={logo} alt="Obsess and Possess Logo" className="" />
+        </Link>
       </div>
-
       <nav className="{styles.nav}">
         <ul>
-          <li> 
+          <li>
             <a href="/">
               <FontAwesomeIcon icon={faHome} />
               {/*alt text home */}
             </a>
-             <h3>test
-            
-             </h3>
-                {/*Tags */}
-                  {/*Tags  */}
-           
+            <h3>test</h3>
+            {/*Tags */}
+            {/*Tags  */}
           </li>
         </ul>
       </nav>
 
- {/*Cart icon  */}
-
-
+      {/*Cart icon  */}
     </header>
   );
 };
