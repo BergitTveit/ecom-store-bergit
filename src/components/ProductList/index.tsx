0fb0c79855ca226bcfeb985acd4f8e3fa4fe3acf
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2024-11-29 14:41:51 +0100

    searchbar with filter on type

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,28 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchProducts } from "../../store/slices/productSlice";
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
 import ProductCard from "../ProductCard";
+import SearchBar from "../Searchbar";
 
 const ProductList = () => {
   const dispatch = useAppDispatch();
   const { products, initialized } = useAppSelector((state) => state.products);
+  const [searchTerm, setSearchTerm] = useState("");
+
   useEffect(() => {
     if (!initialized) {
       dispatch(fetchProducts());
     }
   }, [dispatch, initialized]);
-  useEffect(() => {
-    if (products.length > 0) {
-      const allTags = products.flatMap(product => product.tags);
-      const uniqueTags = [...new Set(allTags)];
-      console.log('All unique tags:', uniqueTags);
-    }
-  }, [products]);
+
+  const filterProducts = products.filter(
+    (product) =>
+      product.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      product.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+    <div>
+      <SearchBar onSearch={setSearchTerm} />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        {(searchTerm ? filterProducts : products).map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -1,2 +1,18 @@
-//searchbar
-export {};
+interface SearchBarProps {
+    onSearch: (searchTerm: string) => void;
+  }
+  
+  const SearchBar = ({ onSearch }: SearchBarProps) => {
+    return (
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Search products..."
+          onChange={(e) => onSearch(e.target.value)}
+          className="w-full max-w-md px-4 py-2 border rounded-lg"
+        />
+      </div>
+    );
+  };
+  
+  export default SearchBar;
