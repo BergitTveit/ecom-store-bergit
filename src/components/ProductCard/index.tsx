9b4f1875c1f2a7152419b3fb2e0171f978191d17
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2024-12-01 20:54:24 +0100

    Add view product button, refactor productprops for better practice

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,19 +1,27 @@
-import { Product } from '../../api/api.types';
+import { Product } from "../../api/api.types";
+import { useNavigate } from "react-router-dom";
 
-interface ProductCardProps {
-  product: Product;
-}
-
-const ProductCard = ({ product }: ProductCardProps) => {
+const ProductCard = ({ product }: { product: Product }) => {
+  const navigate = useNavigate();
+  const handleClick = () => navigate(`/product/${product.id}`);
   return (
     <div className="border rounded-lg p-4 shadow hover:shadow-lg">
-      <img 
-        src={product.image.url} 
-        alt={product.title} 
-        className="w-full h-48 object-cover"
-      />
+      <div onClick={handleClick}>
+        <img
+          src={product.image.url}
+          alt={product.title}
+          className="w-full h-48 object-cover"
+        />
+      </div>
+
       <h2 className="text-lg font-bold mt-2">{product.title}</h2>
       <p className="text-gray-700">${product.price}</p>
+      <button
+        onClick={handleClick}
+        className="mt-4 bg-primary text-white px-4 py-2 rounded-lg w-full"
+      >
+        View Product
+      </button>
     </div>
   );
 };
