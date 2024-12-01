0cc2363be6a45eddb6bc1d56149bb423a57a4187
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2024-12-01 20:54:44 +0100

    Update icon and api file

--- src/api/products.api.ts
+++ src/api/products.api.ts
@@ -1,4 +1,3 @@
-
 import { api } from "./config";
 import { Product, ApiResponse } from "./api.types";
 
--- src/components/CartIcon/index.tsx
+++ src/components/CartIcon/index.tsx
@@ -7,8 +7,8 @@ const CartIcon = () => {
     <div className="flex items-center flex-none ml-auto pl-4 min-w-[40px] relative right-0 gap-6">
       <Link to="/cart">
         <div className="relative cursor-pointer">
-          <FontAwesomeIcon 
-            icon={faShoppingCart} 
+          <FontAwesomeIcon
+            icon={faShoppingCart}
             className="text-lg cursor-pointer text-black hover:text-primary transition-colors"
           />
           <span className="absolute -top-2 -right-2 bg-primary text-white rounded-full px-2 py-0.5 text-xs min-w-4 text-center">
