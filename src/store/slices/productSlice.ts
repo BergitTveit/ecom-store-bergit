98dee4b13bb92a8d9f016c02fe67df4f7a6d53f1
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2024-12-01 11:14:27 +0100

    Add thunk for fetch by id, add reducers fitting for product by id

--- src/store/slices/productSlice.ts
+++ src/store/slices/productSlice.ts
@@ -1,9 +1,11 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 import { productsApi } from "../../api/index";
 import { Product } from "../../api/api.types";
+// import { RejectedAction } from "@reduxjs/toolkit/dist/createAsyncThunk"; Consider installing for rejected actions.
 
 interface ProductState {
   products: Product[];
+  currentProduct: Product | null;
   loading: boolean;
   error: string | null;
   initialized: boolean;
@@ -11,6 +13,7 @@ interface ProductState {
 
 const initialState: ProductState = {
   products: [],
+  currentProduct: null,
   loading: false,
   error: null,
   initialized: false,
@@ -20,21 +23,47 @@ export const fetchProducts = createAsyncThunk("products/fetchAll", async () => {
   return await productsApi.getAll();
 });
 
+export const fetchProductById = createAsyncThunk(
+  "products/fetchById",
+  async (id: string) => {
+    return await productsApi.getById(id);
+  }
+);
+
 const productSlice = createSlice({
   name: "products",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchProducts.pending, (state) => {
+      .addCase(fetchProducts.pending, (state: ProductState) => {
         state.loading = true;
       })
-      .addCase(fetchProducts.fulfilled, (state, action) => {
+      .addCase(
+        fetchProducts.fulfilled,
+        (state: ProductState, action: PayloadAction<Product[]>) => {
+          state.loading = false;
+          state.products = action.payload;
+          state.initialized = true;
+        }
+      )
+      .addCase(fetchProducts.rejected, (state: ProductState, action) => {
         state.loading = false;
-        state.products = action.payload;
-        state.initialized = true;
+        state.error = action.error.message || null;
       })
-      .addCase(fetchProducts.rejected, (state, action) => {
+
+      .addCase(fetchProductById.pending, (state: ProductState) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(
+        fetchProductById.fulfilled,
+        (state: ProductState, action: PayloadAction<Product>) => {
+          state.loading = false;
+          state.currentProduct = action.payload;
+        }
+      )
+      .addCase(fetchProductById.rejected, (state: ProductState, action) => {
         state.loading = false;
         state.error = action.error.message || null;
       });
