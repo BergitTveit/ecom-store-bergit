b67ff16e6cb84a07370ac68c8d479689966a3d9c
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2024-12-11 14:06:28 +0100

    fix config issues for styling

---
+++ src/styles/theme/index.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    primary: "#fe3a64",
+    secondary: "#f090fc",
+    black: "#333333",
+    hover: "#fe3a64",
+    blueForest: "#207178",
+    error: "#dc3545",
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#fe3a64",
-        secondary: "#f090fc",
+        primary: "#075985",
+        secondary: "",
         black: "#333333",
         hover: "#fe3a64",
         blueForest: "#207178",
-        error: "#dc3545",
+        error: "#b91c1c",
       },
       maxWidth: {
         container: "1200px",
