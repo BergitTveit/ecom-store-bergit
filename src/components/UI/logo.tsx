5d3808d2211ee17d635f7994df336a0226030909
BergitTveit <134147170+BergitTveit@users.noreply.github.com>
2024-12-19 08:14:28 +0100

    fix html href to link

--- src/components/UI/logo.tsx
+++ src/components/UI/logo.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export const Logo = () => {
   return (
-    <div className="font-bold text-xl md:text-2xl text-secondary hover:text-accent ">
-      <a href="/" className="flex items-center gap-2">
-      O / P 
-      </a>
+    <div className="font-bold text-xl md:text-2xl text-secondary hover:text-black ">
+      <Link to="/" className="flex items-center gap-2">
+        O / P
+      </Link>
     </div>
   );
 };
